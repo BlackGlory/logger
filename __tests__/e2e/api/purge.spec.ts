60db9fb29418be3ee0d733728e7798e5bd1b4554
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-11-03 18:16:16 +0800

    feat: add /api/logger/:id/purge

---
+++ __tests__/e2e/api/purge.spec.ts
@@ -0,0 +1,69 @@
+import { buildServer } from '@src/server'
+import { prepareDatabase, resetEnvironment } from '@test/utils'
+import { matchers } from 'jest-json-schema'
+
+jest.mock('@dao/access-control/database')
+jest.mock('@dao/json-schema/database')
+jest.mock('@dao/logger/database')
+expect.extend(matchers)
+
+beforeEach(async () => {
+  resetEnvironment()
+  await prepareDatabase()
+})
+
+describe('Purge', () => {
+  describe('POST /api/logger/:id/purge', () => {
+    describe('auth', () => {
+      it('204', async () => {
+        process.env.LOGGER_ADMIN_PASSWORD = 'password'
+        const server = await buildServer()
+        const id = 'id'
+
+        const res = await server.inject({
+          method: 'POST'
+        , url: `/api/logger/${id}/purge`
+        , headers: createAuthHeaders()
+        })
+
+        expect(res.statusCode).toBe(204)
+      })
+    })
+
+    describe('no admin password', () => {
+      it('401', async () => {
+        const server = await buildServer()
+        const id = 'id'
+
+        const res = await server.inject({
+          method: 'POST'
+        , url: `/api/logger/${id}/purge`
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+
+    describe('bad auth', () => {
+      it('401', async () => {
+        process.env.LOGGER_ADMIN_PASSWORD = 'password'
+        const server = await buildServer()
+        const id = 'id'
+
+        const res = await server.inject({
+          method: 'POST'
+        , url: `/api/logger/${id}/purge`
+        , headers: createAuthHeaders('bad')
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+  })
+})
+
+function createAuthHeaders(adminPassword?: string) {
+  return {
+    'Authorization': `Bearer ${ adminPassword ?? process.env.LOGGER_ADMIN_PASSWORD }`
+  }
+}
