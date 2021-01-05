baefdba74db27f627d10192662298845371c7218
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-05 11:45:11 +0800

    fix: schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,11 +1,11 @@
 export const tokenSchema = {
   type: 'string'
-, pattern: '^[a-zA-Z0-9\\.\\-_]{1,256}$'
+, pattern: '^[a-zA-Z0-9\\.\\-_]{0,255}$'
 }
 
 export const idSchema = {
   type: 'string'
-, pattern: '^[a-zA-Z0-9\\.\\-_]{1,256}$'
+, pattern: '^[a-zA-Z0-9\\.\\-_]{0,255}$'
 }
 
 export const logIdSchema = {
