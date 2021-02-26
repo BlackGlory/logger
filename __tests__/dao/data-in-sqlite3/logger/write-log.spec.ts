1629b04048098fddc3e762334e843d0c016e8314
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-02-26 14:32:57 +0000

    style: update

--- __tests__/dao/data-in-sqlite3/logger/write-log.spec.ts
+++ __tests__/dao/data-in-sqlite3/logger/write-log.spec.ts
@@ -35,8 +35,8 @@ describe('writeLog(id: string, payload: string): string', () => {
         expect(result1).toBe(`${timestamp}-0`)
         expect(result2).toBe(`${timestamp}-1`)
         expect(rows).toMatchObject([
-          { logger_id: id, payload: payload1, timestamp, number: 0 }
-        , { logger_id: id, payload: payload2, timestamp, number: 1 }
+          { payload: payload1, timestamp, number: 0 }
+        , { payload: payload2, timestamp, number: 1 }
         ])
       })
     })
@@ -57,9 +57,9 @@ describe('writeLog(id: string, payload: string): string', () => {
 
         expect(result1).toEqual(`${timestamp1}-0`)
         expect(result2).toEqual(`${timestamp2}-0`)
-        expect(rows).toEqual([
-          { logger_id: id, payload: payload1, timestamp: timestamp1, number: 0 }
-        , { logger_id: id, payload: payload2, timestamp: timestamp2, number: 0 }
+        expect(rows).toMatchObject([
+          { payload: payload1, timestamp: timestamp1, number: 0 }
+        , { payload: payload2, timestamp: timestamp2, number: 0 }
         ])
       })
     })
