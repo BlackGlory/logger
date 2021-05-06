fa6565898be28e1febea5eeb119166bf33b3f1f1
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-06 11:06:27 +0000

    refactor: update

--- src/services/logger/query.ts
+++ src/services/logger/query.ts
@@ -1,4 +1,3 @@
-import { go } from '@blackglory/go'
 import { FastifyPluginAsync } from 'fastify'
 import { namespaceSchema, tokenSchema, logIdSchema } from '@src/schema'
 import { Readable } from 'stream'
@@ -33,42 +32,40 @@ export const routes: FastifyPluginAsync<{ Core: ICore }> = async function routes
         }
       }
     }
-  , (req, reply) => {
-      go(async () => {
-        const namespace = req.params.namespace
-        const token = req.query.token
-        const range: IRange = {
-          from: req.query.from
-        , to: req.query.to
-        }
-        if (req.query.head) (range as ISlice & IHead).head = req.query.head
-        if (req.query.tail) (range as ISlice & ITail).tail = req.query.tail
+  , async (req, reply) => {
+      const namespace = req.params.namespace
+      const token = req.query.token
+      const range: IRange = {
+        from: req.query.from
+      , to: req.query.to
+      }
+      if (req.query.head) (range as ISlice & IHead).head = req.query.head
+      if (req.query.tail) (range as ISlice & ITail).tail = req.query.tail
 
-        try {
-          await Core.Blacklist.check(namespace)
-          await Core.Whitelist.check(namespace)
-          await Core.TBAC.checkReadPermission(namespace, token)
-        } catch (e) {
-          if (e instanceof Core.Blacklist.Forbidden) return reply.status(403).send()
-          if (e instanceof Core.Whitelist.Forbidden) return reply.status(403).send()
-          if (e instanceof Core.TBAC.Unauthorized) return reply.status(401).send()
-          throw e
-        }
+      try {
+        await Core.Blacklist.check(namespace)
+        await Core.Whitelist.check(namespace)
+        await Core.TBAC.checkReadPermission(namespace, token)
+      } catch (e) {
+        if (e instanceof Core.Blacklist.Forbidden) return reply.status(403).send()
+        if (e instanceof Core.Whitelist.Forbidden) return reply.status(403).send()
+        if (e instanceof Core.TBAC.Unauthorized) return reply.status(401).send()
+        throw e
+      }
 
-        const logs = Core.Logger.query(namespace, range)
-        const accept = req.accepts().type(['application/json', 'application/x-ndjson'])
-        if (accept === 'application/x-ndjson') {
-          reply
-            .status(200)
-            .header('Content-Type', 'application/x-ndjson')
-            .send(Readable.from(stringifyNDJSONStreamAsync(logs)))
-        } else {
-          reply
-            .status(200)
-            .header('Content-Type', 'application/json')
-            .send(Readable.from(stringifyJSONStreamAsync(logs)))
-        }
-      })
+      const logs = Core.Logger.query(namespace, range)
+      const accept = req.accepts().type(['application/json', 'application/x-ndjson'])
+      if (accept === 'application/x-ndjson') {
+        reply
+          .status(200)
+          .header('Content-Type', 'application/x-ndjson')
+          .send(Readable.from(stringifyNDJSONStreamAsync(logs)))
+      } else {
+        reply
+          .status(200)
+          .header('Content-Type', 'application/json')
+          .send(Readable.from(stringifyJSONStreamAsync(logs)))
+      }
     }
   )
 }
